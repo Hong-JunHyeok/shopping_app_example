0c58dad50f76b7fdfbbee8bd6e6d8bef575b6ccf
Hong-JunHyeok <edb1631@naver.com>
2023-06-18 18:48:43 +0900

    동적 import 하기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,29 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { Layout } from './components/shared';
 
-import { ProductPage, CartPage, PurchasePage, HomePage, ProductCreatePage, NotFoundPage } from './pages';
+//NOTE: 동적 import 관련한 설명하기
+const ProductPage = React.lazy(() => import('./pages/ProductPage'));
+const CartPage = React.lazy(() => import('./pages/CartPage'));
+const PurchasePage = React.lazy(() => import('./pages/PurchasePage'));
+const HomePage = React.lazy(() => import('./pages/HomePage'));
+const ProductCreatePage = React.lazy(() => import('./pages/ProductCreatePage'));
+const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
 
 function App() {
- return (
-   <Layout>
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="product/:productId" element={<ProductPage />}/>
-      <Route path="purchase/:productId" element={<PurchasePage />} />
-      <Route path="create" element={<ProductCreatePage />} />
-      <Route path="cart" element={<CartPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-   </Layout>
- );
+  return (
+    <Layout>
+      <Routes>
+        <Route index element={<React.Suspense fallback={<div>Loading...</div>}><HomePage /></React.Suspense>} />
+        <Route path="product/:productId" element={<React.Suspense fallback={<div>Loading...</div>}><ProductPage /></React.Suspense>} />
+        <Route path="purchase/:productId" element={<React.Suspense fallback={<div>Loading...</div>}><PurchasePage /></React.Suspense>} />
+        <Route path="create" element={<React.Suspense fallback={<div>Loading...</div>}><ProductCreatePage /></React.Suspense>} />
+        <Route path="cart" element={<React.Suspense fallback={<div>Loading...</div>}><CartPage /></React.Suspense>} />
+        <Route path="*" element={<React.Suspense fallback={<div>Loading...</div>}><NotFoundPage /></React.Suspense>} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
