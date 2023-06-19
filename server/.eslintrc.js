983c163148e151af838e516d205bd40836a0e249
Hong-JunHyeok <edb1631@naver.com>
2023-06-19 22:40:47 +0900

    backend패키지 업데이트

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
+    project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
     sourceType: "module",
   },
