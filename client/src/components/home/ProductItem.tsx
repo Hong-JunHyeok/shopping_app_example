1fbec9316454f676fcc3413f6db0991ce122b36d
Hong-JunHyeok <edb1631@naver.com>
2023-07-08 18:21:34 +0900

    [section/6] ProductItem/ProductList MUI 마이그레이션

--- client/src/components/home/ProductItem.tsx
+++ client/src/components/home/ProductItem.tsx
@@ -1,77 +1,69 @@
-// ProductItem.tsx
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { ProductType } from "../../types";
+  // ProductItem.tsx
+  import { Box, Button, Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+  import { useNavigate } from "react-router-dom";
 
-interface ProductItemProps {
-  product: ProductType;
-  onDelete: (id: string) => void;
-  onUpdate: (product: ProductType) => void;
-}
+  import { ProductType } from "../../types";
 
-function ProductItem({ product, onDelete, onUpdate }: ProductItemProps) {
-  const { id, name, price, explanation } = product;
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [editName, setEditName] = useState(product.name);
-  const [editExplanation, setEditExplanation] = useState(product.explanation);
-  const [editPrice, setEditPrice] = useState(product.price);
 
-  return (
-    <div>
-      {product.thumbnail && (
-        <img 
-          src={product.thumbnail}
-        />
-      )}
-      <div>
-        <Link to={`/${id}`}>{name}</Link>
-      </div>
-      <div>{price}</div>
-      <div>{explanation}</div>
+  interface ProductItemProps {
+    product: ProductType;
+  }
 
-      <button type="button" onClick={() => onDelete(id)}>
-        삭제하기
-      </button>
+  function ProductItem({ product }: ProductItemProps) {
+    const navigate = useNavigate();
 
-      <button type="button" onClick={() => setIsEditMode((prev) => !prev)}>
-        수정하기
-      </button>
+    const handlePushProductPage = () => navigate(`/product/${product.id}`);
+    const handlePushPurchasePage = () => navigate(`/purchase/${product.id}`)
 
-      {isEditMode && (
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            onUpdate({
-              id,
-              name: editName,
-              price: editPrice,
-              explanation: editExplanation
-            });
-          }}
-        >
-          <input
-            type="text"
-            placeholder="상품의 이름"
-            value={editName}
-            onChange={(event) => setEditName(event.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="상품 설명"
-            value={editExplanation}
-            onChange={(event) => setEditExplanation(event.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="상품 가격"
-            value={editPrice}
-            onChange={(event) => setEditPrice(parseInt(event.target.value, 10))}
-          />
-          <input type="submit" value="상품 수정하기" />
-        </form>
-      )}
-    </div>
-  );
-}
+    return (
+      <Grid item xs={12} sm={6} md={4}>
+        <Card sx={{ maxWidth: 345, padding: 3, height: 300 }} onClick={handlePushProductPage}>
+            {product.thumbnail && (
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image={product.thumbnail}
+                    title={product.name}
+                />
+            )}
+            <CardContent sx={{ padding: 0 }}>
+              <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  sx={{
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      whiteSpace: 'nowrap',
+                  }}
+              >
+                  {product.name}
+              </Typography>
+              <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{
+                      height: 30,
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      whiteSpace: 'nowrap',
+                  }}
+              >
+                  {product.explanation}
+              </Typography>
+            </CardContent>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: 3 }}>
 
-export default ProductItem;
+            <Button
+                size="small"
+                onClick={handlePushPurchasePage}
+                variant="contained"
+            >
+                구매하기
+            </Button>
+            </Box>
+        </Card>
+      </Grid>
+    );
+  }
+
+  export default ProductItem;
--- client/src/components/home/ProductList.tsx
+++ client/src/components/home/ProductList.tsx
@@ -1,5 +1,5 @@
 // ProductList.tsx
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, Grid } from "@mui/material";
 import { useEffect, useState } from "react";
 import { ProductItem } from ".";
 import { ProductType } from "../../types";
@@ -7,34 +7,6 @@ import { ProductType } from "../../types";
 const ProductList = () => {
  const [products, setProducts] = useState<ProductType[]>([]);
  const [isLoading, setIsLoading] = useState(false);
-
- const handleDelete = (id: string) => {
-   fetch(`/product/${id}`, {
-     method: "DELETE",
-   }).then((response) => {
-     if (response.ok) {
-       setProducts(products.filter((product) => product.id !== id));
-     }
-   });
- };
-
- const handleUpdate = (updateProduct: ProductType) => {
-   fetch(`/product/${updateProduct.id}`, {
-     method: "PATCH",
-     headers: {
-       "Content-Type": "application/json",
-     },
-     body: JSON.stringify(updateProduct),
-   }).then((response) => {
-     if (response.ok) {
-       setProducts(
-         products.map((product) =>
-           product.id === updateProduct.id ? updateProduct : product
-         )
-       );
-     }
-   });
- };
 
  useEffect(() => {
   setIsLoading(true);
@@ -48,15 +20,14 @@ const ProductList = () => {
  if (isLoading) return <CircularProgress />;
 
  return (
-   <ul>
-     {products.map((product) => (
-       <ProductItem
-         product={product}
-         onDelete={handleDelete}
-         onUpdate={handleUpdate}
-       />
-     ))}
-   </ul>
+    <Grid container spacing={3}>
+      {products.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={product}
+          />
+      ))}
+    </Grid>
  );
 };
 
