c6dfc4f6235b69caa4ff298bb6c3a1d86f4c1acf
Hong-JunHyeok <edb1631@naver.com>
2023-07-25 23:06:47 +0900

    포트에 대한 설명 제거

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import searchRouter from './routes/search';
 
 config();
 
-if (!process.env.PORT) throw Error('포트 번호 환경변수 설정을 해주세요.');
+const PORT = 3090 as const;
 
 if (!fs.existsSync('thumbnails')) {
   console.log('thumbnails 폴더가 없으므로 만듭니다...');
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 app.use('/product', productRouter);
 app.use('/search', searchRouter);
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line
-  console.log(`Server is running at http://localhost:${process.env.PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
