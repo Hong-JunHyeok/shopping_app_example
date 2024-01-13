67c0486597b6baee2716eb76a60ec3019f9ca6c0
Hong-JunHyeok <edb1631@naver.com>
2024-01-13 20:35:17 +0900

    [section/2] 최종 코드

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,13 @@
-import { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { ErrorBoundary } from 'react-error-boundary';
-
-import App from './App';
-
-import { ErrorPage, LoadingPage } from './pages';
+// index.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-
 root.render(
-    <BrowserRouter>
-      <ErrorBoundary FallbackComponent={ErrorPage}>
-        <Suspense fallback={<LoadingPage />}>
-          <App /> 
-        </Suspense>
-      </ErrorBoundary>
-    </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
--- client/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export interface ProductType {
-    id: string;
-    name: string;
-    explanation: string;
-    thumbnail: string | null;
-    price: number;
-};
