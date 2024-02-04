31d6ae37b8c6c4df798a887f234f99db462f0f89
Hong-JunHyeok <edb1631@naver.com>
2024-02-04 15:23:50 +0900

    [section/7] Server thumbnail 수정

--- server/src/model/products.ts
+++ server/src/model/products.ts
@@ -14,8 +14,7 @@ const products: ProductType[] = [
     explanation:
       "디스플레이는 6.1인치 19.5:9 비율의 2532×1170 \n    해상도를 지원하며 패널 형식은 AMOLED 방식의 Super Retina XDR 디스플레이이다. \n    인치당 픽셀 수는 460 ppi이다. 120Hz의 터치 샘플링 레이트를 제공하고 명암비는 2,000,000:1이다",
     price: 1230000,
-    thumbnail:
-      "https://img.etnews.com/news/article/2021/08/30/cms_temp_article_30102128407144.jpg",
+    thumbnail: "thumbnails/1531084981889-56574410.jpeg",
   },
   {
     id: "de8a0304-ea0a-42f5-b908-986869a49e6d",
