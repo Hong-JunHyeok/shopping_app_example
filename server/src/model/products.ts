77326654a09569cf7b05a9ff7d611f8c80f60494
Hong-JunHyeok <edb1631@naver.com>
2024-02-04 15:27:05 +0900

    Server value 수정

--- server/src/model/products.ts
+++ server/src/model/products.ts
@@ -9,15 +9,6 @@ export interface ProductType {
 
 const products: ProductType[] = [
   {
-    id: "9e31dbd9-b871-43ef-ad5d-80411c4eaa48",
-    name: "Iphone 13 Max",
-    explanation:
-      "디스플레이는 6.1인치 19.5:9 비율의 2532×1170 \n    해상도를 지원하며 패널 형식은 AMOLED 방식의 Super Retina XDR 디스플레이이다. \n    인치당 픽셀 수는 460 ppi이다. 120Hz의 터치 샘플링 레이트를 제공하고 명암비는 2,000,000:1이다",
-    price: 1230000,
-    thumbnail:
-      "https://img.etnews.com/news/article/2021/08/30/cms_temp_article_30102128407144.jpg",
-  },
-  {
     id: "de8a0304-ea0a-42f5-b908-986869a49e6d",
     name: "스타벅스 기프티콘",
     explanation:
