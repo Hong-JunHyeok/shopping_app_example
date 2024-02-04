29fe40155d9af4a6cd3182a78dd5538d2a9aff5b
Hong-JunHyeok <edb1631@naver.com>
2024-02-04 13:22:57 +0900

    [section/6] 상품 추가 모달 로직 추가

--- client/src/components/create/ProductCreateForm.tsx
+++ client/src/components/create/ProductCreateForm.tsx
@@ -1,6 +1,17 @@
 // ProductCreateForm.tsx
-import { Button, Container, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { ThumbnailUploader } from ".";
 import { ProductType } from "../../types";
 
@@ -9,6 +20,10 @@ const ProductCreateForm = () => {
   const [price, setPrice] = useState(0);
   const [explanation, setExplanation] = useState("");
   const [thumbnail, setThumbnail] = useState<File | null>(null);
+  const [createdProductId, setCreatedProductId] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
@@ -56,6 +71,14 @@ const ProductCreateForm = () => {
     if (thumbnail) {
       await uploadThumbnailRequest(data.product.id, thumbnail);
     }
+
+    setCreatedProductId(data.product.id);
+    setIsModalOpen(true);
+  };
+
+  const handlePushProductPage = () => {
+    setIsModalOpen(false);
+    navigate(`/product/${createdProductId}`);
   };
 
   return (
@@ -106,6 +129,26 @@ const ProductCreateForm = () => {
           </Button>
         </form>
       </Container>
+
+      <Dialog
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">
+          상품을 성공적으로 추가했습니다.
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            확인을 누르면 상품상세 페이지로 이동합니다.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handlePushProductPage} autoFocus>
+            확인
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
