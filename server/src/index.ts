5d2b149889e604017c37d1dbd32a0aea45394c54
Hong-JunHyeok <edb1631@naver.com>
2024-04-11 17:52:28 +0900

    server port update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,30 +1,30 @@
-import express from 'express';
-import { config } from 'dotenv';
-import fs from 'fs';
+import express from "express";
+import { config } from "dotenv";
+import fs from "fs";
 
-import productRouter from './routes/product';
-import searchRouter from './routes/search';
+import productRouter from "./routes/product";
+import searchRouter from "./routes/search";
 
 config();
 
-const PORT = 3090 as const;
+const PORT = process.env.PORT || 3090;
 
-if (!fs.existsSync('thumbnails')) {
-  console.log('thumbnails 폴더가 없으므로 만듭니다...');
-  fs.mkdirSync('thumbnails');
+if (!fs.existsSync("thumbnails")) {
+  console.log("thumbnails 폴더가 없으므로 만듭니다...");
+  fs.mkdirSync("thumbnails");
 }
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/thumbnails', express.static('thumbnails'));
+app.use("/thumbnails", express.static("thumbnails"));
 
-app.get('/', (req, res) => {
-  res.send('Shopping App API server');
+app.get("/", (req, res) => {
+  res.send("Shopping App API server");
 });
-app.use('/product', productRouter);
-app.use('/search', searchRouter);
+app.use("/product", productRouter);
+app.use("/search", searchRouter);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line
